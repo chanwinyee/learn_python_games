cea2e5806b1cbd7c8ec37d867a49dc2707985136
Elizabeth Chan <chanwinyee@elizabesmacbook.gateway.sonic.net>
2020-01-24 07:41:45 -0800

    Cleaned up hangman code and it is now operational

--- hangman.py
+++ hangman.py
@@ -35,12 +35,12 @@ HANGMAN_PICS = ['''
    /|\  |
    / \  |
        ===''']
-words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.split()
+words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.split()
 
 def getRandomWord(wordList):
         # This function returns a random string from the passed list of strings.
-        wordIndex = random.randint(0,len(wordList) -1) # Arrays start at 0
-        return wordList[wordIndex] # the value in the brackets is the position number
+    wordIndex = random.randint(0,len(wordList) -1) # Arrays start at 0
+    return wordList[wordIndex] # the value in the brackets is the position number
 
 def displayBoard(missedLetters, correctLetters, secretWord):
     print(HANGMAN_PICS[len(missedLetters)])
@@ -53,17 +53,17 @@ def displayBoard(missedLetters, correctLetters, secretWord):
 
     blanks = '_' * len(secretWord)
 
-    for i in range(len(secretWord)): #Replace blanks with correctly guessed letters.
+    for i in range(len(secretWord)): # replace blanks with correctly guessed letters.
         if secretWord[i] in correctLetters:
             blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
 
-    for letter in blanks: #Show the secret word with spaces in between each letter.
+    for letter in blanks: # Show the secret word with spaces in between each letter.
         print(letter, end=' ')
-    print() 
+    print()
 
 def getGuess(alreadyGuessed):
-    # Returns the ltter the player entered. This function makes sure the player entered a single letter and not something else.
-    while True: 
+    # Returns the letter the player entered. This function makes sure the player entered a single letter and not something else.
+    while True:
         print('Guess a letter.')
         guess = input()
         guess = guess.lower()
@@ -74,45 +74,46 @@ def getGuess(alreadyGuessed):
         elif guess not in 'abcdefghijklmnopqrstuvwxyz':
             print('Please enter a LETTER.')
         else:
-            return guess 
+            return guess
 
 def playAgain():
-    # This function returns True if the player wants to play again; otherwise, it returns false.
+    # This function returns True if the player wants to play again; otherwise, it returns False.
     print('Do you want to play again? (yes or no)')
     return input().lower().startswith('y')
+
 
 print ('H A N G M A N')
 missedLetters = ''
 correctLetters = ''
-secretWord = getRandomWord(words) 
-gameIsDone = False 
+secretWord = getRandomWord(words)
+gameIsDone = False
 
 while True:
     displayBoard(missedLetters, correctLetters, secretWord)
 
-    # Let the player enter a letter
+    # Let the player enter a letter.
     guess = getGuess(missedLetters + correctLetters)
 
     if guess in secretWord:
-        correctLetters = correctLetters + guess 
+        correctLetters = correctLetters + guess
 
         # Check if the player has won.
-        foundAllLetters = True 
+        foundAllLetters = True
         for i in range(len(secretWord)):
             if secretWord[i] not in correctLetters:
-                foundAllLetters = False 
-                break 
+                foundAllLetters = False
+                break
             if foundAllLetters:
                 print('Yes! The secret word is "' + secretWord + '"! You have won!')
-                gameIsDone = True 
-        else: 
-            missedLetter = missedLetters + guess 
+                gameIsDone = True
+    else:
+        missedLetters = missedLetters + guess
 
-            # Check if player has guessed too many times and lost.
-            if len(missedLetters) == len(HANGMAN_PICS) - 1:
+        # Check if player has guessed too many times and lost.
+        if len(missedLetters) == len(HANGMAN_PICS) - 1:
                 displayboard(missedLetters, correctLetters, secretWord)
                 print('You have run out of guesses!\nAfter ' + str(len(missedLetters)) + 'missed guesses and ' + str(len(correctLetters)) + 'correct guesses, the word was "' + secretWord + '"')
-                gameIsDone = True 
+                gameIsDone = True
 
         # Ask the player if they want to play again (but only if the game is done).
         if gameIsDone:
@@ -122,4 +123,5 @@ while True:
                     gameIsDone = False
                     secretWord = getRandomWord(words)
                 else:
-                    break 
+                    break
+# The spacing within the code matters
--- playground.py
+++ playground.py
@@ -1,7 +1,49 @@
-print('Set a range for your "For" loop.')
-set_range = int(input())
+# print('Set a range for your "For" loop.')
+# set_range = int(input())
 
-for i in range(set_range):
-    print('Hello! i is set to', i)
+# for i in range(set_range):
+#     print('Hello! i is set to', i)
 
-print('This is the end of the loop.')
+# print('This is the end of the loop.')
+
+
+HANGMAN_PICS = ['''
+    +---+
+        |
+        |
+        |
+       ===''','''
+    +---+
+    O   |
+        |
+        |
+       ===''','''
+    +---+
+    O   |
+    |   |
+        |
+       ===''','''
+    +---+
+    O   |
+   /|   |
+        |
+       ===''','''
+    +---+
+    O   |
+   /|\  |
+        |
+       ===''','''
+    +---+
+    O   |
+   /|\  |
+   /    |
+    ===''','''
+    +---+
+    0   |
+   /|\  |
+   / \  |
+       ===''']
+
+print(HANGMAN_PICS[0])
+print(HANGMAN_PICS[3])
+print(HANGMAN_PICS[6])
