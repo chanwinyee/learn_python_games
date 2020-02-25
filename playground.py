579968bac6f7679d930e835e59f9d957578e91d8
Elizabeth Chan <chanwinyee@elizabesmacbook.gateway.sonic.net>
2020-02-25 06:35:29 -0800

    finished hangman chapter

--- playground.py
+++ playground.py
@@ -7,43 +7,54 @@
 # print('This is the end of the loop.')
 
 
-HANGMAN_PICS = ['''
-    +---+
-        |
-        |
-        |
-       ===''','''
-    +---+
-    O   |
-        |
-        |
-       ===''','''
-    +---+
-    O   |
-    |   |
-        |
-       ===''','''
-    +---+
-    O   |
-   /|   |
-        |
-       ===''','''
-    +---+
-    O   |
-   /|\  |
-        |
-       ===''','''
-    +---+
-    O   |
-   /|\  |
-   /    |
-    ===''','''
-    +---+
-    0   |
-   /|\  |
-   / \  |
-       ===''']
+# HANGMAN_PICS = ['''
+#     +---+
+#         |
+#         |
+#         |
+#        ===''','''
+#     +---+
+#     O   |
+#         |
+#         |
+#        ===''','''
+#     +---+
+#     O   |
+#     |   |
+#         |
+#        ===''','''
+#     +---+
+#     O   |
+#    /|   |
+#         |
+#        ===''','''
+#     +---+
+#     O   |
+#    /|\  |
+#         |
+#        ===''','''
+#     +---+
+#     O   |
+#    /|\  |
+#    /    |
+#     ===''','''
+#     +---+
+#     0   |
+#    /|\  |
+#    / \  |
+#        ===''']
 
-print(HANGMAN_PICS[0])
-print(HANGMAN_PICS[3])
-print(HANGMAN_PICS[6])
+# print(HANGMAN_PICS[0])
+# print(HANGMAN_PICS[3])
+# print(HANGMAN_PICS[6])
+
+# animals = ['aardvark','anteater','antelope','albert']
+# print(animals)
+# print(animals[0])
+
+sentence = 'I am a big cat, who eats a lot of cans'
+words = sentence.split()
+print(words)
+sentence=input()
+words = words + sentence.split()
+print(words)
